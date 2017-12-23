360e7da6bb0da966a8fd89ae66351074ea473e88
Miles Steele <miles@milessteele.com>
2017-12-22 22:48:07 -0500

    q10 part2

--- q10/src/main.rs
+++ q10/src/main.rs
@@ -4,8 +4,10 @@ use std::fmt;
 type Result<T> = std::result::Result<T, String>;
 
 #[allow(dead_code)]
-fn e<T>(msg: &str) -> Result<T> {
-    return Err(msg.to_owned());
+fn e<T,S>(msg: S) -> Result<T>
+    where S: Borrow<String>
+{
+    return Err(msg.borrow().to_string());
 }
 
 fn main() {
@@ -17,8 +19,8 @@ fn main() {
 
 fn main2() -> Result<()> {
     let intxt = read_file("input.txt")?;
-    let instructions = parse(&intxt)?;
-    let answer = q10p1(instructions)?;
+    let instructions = parse_p2(&intxt)?;
+    let answer = q10p2(instructions)?;
     println!("{}", answer);
     Ok(())
 }
@@ -36,13 +38,27 @@ fn read_file(path: &str) -> Result<String> {
     Ok(contents)
 }
 
-fn parsep1(s: &str) -> Result<Vec<i64>> {
+#[allow(dead_code)]
+fn parse_p1(s: &str) -> Result<Vec<i64>> {
     let mut res = Vec::new();
     for x in s.trim().lines()
         .next().ok_or_else(|| "no input line")?
         .split(',')
     {
         res.push(x.trim().parse().map_err(|_| "unexpected non-integer")?);
+    }
+    Ok(res)
+}
+
+fn parse_p2(s: &str) -> Result<Vec<u8>> {
+    let mut res = Vec::new();
+    if let Some(line) = s.trim().lines().next() {
+        for c in line.trim().chars() {
+            if !c.is_ascii() {
+                return e(format!("non-ascii char: {}", c))
+            }
+            res.push(c as u8)
+        }
     }
     Ok(res)
 }
@@ -57,6 +73,53 @@ fn q10p1(lengths: Vec<i64>) -> Result<i64> {
         println!("{} -> {:?}", l, s);
     }
     Ok(s.list[0] * s.list[1])
+}
+
+#[allow(dead_code)]
+fn q10p2(mut lengths: Vec<u8>) -> Result<String> {
+    println!("{:?}", lengths);
+    let mut suffix = vec![17,31,73,47,23];
+    lengths.append(&mut suffix);
+    let mut s = State::new(256);
+    println!("{:?}", s);
+    for round in 0..64 {
+        println!("round: {}", round);
+        for &l in lengths.iter() {
+            step(&mut s, l as i64)?;
+            println!("{} -> {:?}", l, s);
+            assert_eq!(256, s.list.len());
+        }
+    }
+    let compact = compact_hash(conv(s.list)?);
+    Ok(hex(compact))
+}
+
+fn compact_hash(xs: Vec<u8>) -> Vec<u8> {
+    xs.chunks(16).map(|chunk| {
+        chunk.iter().fold(0, |acc, x| {
+            acc ^ x
+        })
+    }).collect()
+}
+
+fn hex(xs: Vec<u8>) -> String {
+    xs.iter().fold(String::with_capacity(xs.len() * 2), |mut acc, x| {
+        acc.push_str(&format!("{:02x}", x));
+        acc
+    })
+}
+
+fn conv<V>(xs: V) -> Result<Vec<u8>>
+    where V: Borrow<Vec<i64>>
+{
+    let mut res = Vec::new();
+    for x in xs.borrow().iter().cloned() {
+        if x < 0 || x >= 256 {
+            return e(format!("non-u8 value: {}", x))
+        }
+        res.push(x as u8);
+    }
+    Ok(res)
 }
 
 struct State {
@@ -117,6 +180,13 @@ fn reverse_cyclic<T,V>(v: V, start: usize, len: usize) -> Vec<T>
 }
 
 #[test]
-fn it_works() {
+fn p1() {
     assert_eq!(vec![4,3,2,1,0], reverse_cyclic(vec![0,1,2,3,4], 3, 4));
+}
+
+#[test]
+fn p2() {
+    assert_eq!("1111", hex(vec!(17,17)));
+    assert_eq!("0000", hex(vec!(0,0)));
+    assert_eq!("a2582a3a0e66e6e86e3812dcb672a272", q10p2(vec![]).unwrap());
 }
